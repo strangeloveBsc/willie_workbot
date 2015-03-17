fb35c219110fcd1cc808967f320bc366c30ed5ff
Mike <mike@Ogmar>
2015-03-17 11:23:57 +0100

    Added some features, established a working version

--- uptime.py
+++ uptime.py
@@ -7,7 +7,6 @@ import schedule
 import os.path
 import pickle
 
-# Verwendung der Klasse ist noch ungetestet!!
 class botStatus:
     def __init__(self):
         self.active_list = {} 	# wer ist da
@@ -19,6 +18,7 @@ class botStatus:
 
 friends = {"name1":"greeting1", "name2":"greeting2"}
 today = datetime.date
+botswana = botStatus()
 
 def check():
     for user in active_list.keys():
@@ -26,7 +26,7 @@ def check():
         willie.msg(user, "Bitte trage deine Arbeitszeit für gestern nach.")
         willie.msg(user, "addtime <Stunden>:<Minuten>")
     active_list.clear()
-    if(datetime.date.weekday == 0):  # was 0
+    if(datetime.date.weekday == 0): 
         for user in botswana.working_minutes.keys():
             if (user in week_list):
                 botswana.pending_messages[user].append("Wochenresumee zum Montagmorgen:")
@@ -50,15 +50,37 @@ def seconds2time(seconds):
     h, m = divmod(m, 60)
     return "%dh %02dm" % (h, m)
 
-# this should become obsolete
-# also new format needs to be enforced
+# This method is only used for week-hours
 def check4int(hours):
 	try:
 		i_hours = int(hours)
 		return i_hours
-	except TypeError:
-		willie.msg(senderl, " bitte gib eine ganze Zahl an!")
-		return 0
+	except ValueError:
+		return -1
+
+# This method is used to add time on the schedule
+def check4time(tstring):
+    ret = []
+    try:
+        pos = tstring.find(":")
+    # Python has no switch/case. This is uuugly as hell :/
+        if(pos == -1): # hours only
+            hours = int(tstring)
+            minutes = 0
+        else:
+            if (pos == 0): # minutes only
+                hours = 0
+                minutes = int(tstring[1:])
+            else:
+                hours = int(tstring[:pos])
+                minutes = int(tstring[pos+1:])
+        ret.append(hours)
+        ret.append(minutes)
+        print(ret)
+        return ret
+    except ValueError:
+        return []
+        
 
 @willie.module.nickname_commands('addtime')
 #TODO: Regex hinzufügen
@@ -66,8 +88,19 @@ def check4int(hours):
 def addtime(willie, trigger):
     sender_l = (str(trigger.nick)).lower()
     
-    seconds = check4int(trigger.group(3)) * 3600 # get seconds
-    working_minutes[sender_l] += seconds
+    if(sender_l not in botswana.working_minutes):
+        willie.msg(sender_l, "Sorry, members only!")
+        return
+    
+    if(trigger.group(3) is None):
+        willie.msg(sender_l, "addtime <Stunden>[:<Minuten>]")
+        return
+    time2add = check4time(trigger.group(3))
+    if(len(time2add) < 2):
+        willie.msg(sender_l, "Das mit dem Input üben wir aber nochmal!")
+        return
+    seconds = time2add[0] * 3600 + time2add[1] * 60
+    botswana.working_minutes[sender_l] += seconds
     if(seconds > 0):
         willie.msg(sender_l, "Dein Zeitkonto wurde auf " + seconds2time(botswana.working_minutes[sender_l]) + " aufgestockt")
     else:
@@ -75,23 +108,26 @@ def addtime(willie, trigger):
 	
 @willie.module.nickname_commands('wtime')
 def wtime(willie, trigger):
-    user_l = (str(trigger.nick)).lower()
-# TODO: almost-obsolete check4int function
+    sender_l = (str(trigger.nick)).lower()
+    if(trigger.group(3) is None):
+        if(sender_l in botswana.week_list):
+            willie.msg(sender_l, "Aktuell hast du bei mir " + str(botswana.week_list[sender_l] / 3600) + " Stunden vermerkt")
+        else:
+            willie.msg(sender_l, "Tut mir leid, kein Eintrag für dich vorhanden!")
+        return
     ret = check4int(trigger.group(3))
     if(ret > 0):
-        botswana.week_list[user_l] = ret * 3600
-        willie.msg(user_l, "Du hast " + str(ret) + " Wochenstunden")
+        botswana.week_list[sender_l] = ret * 3600
+        willie.msg(sender_l, "Du hast " + str(ret) + " Wochenstunden")
     else:
-        willie.msg(user_l, ret + " ist keine gültige Zeit :(")
-
-
+        willie.msg(sender_l, "Bitte gib ein x:{x∈ℕ} an!")
 
 @willie.module.nickname_commands('moin', 'Moin', 'tach', 'hi', 'Tach', 'Hi', 'Hey')
 def add(willie, trigger):
     sender = str(trigger.nick)
     sender_l = sender.lower()
 
-    if(sender_l in active_list.keys()):
+    if(sender_l in botswana.active_list.keys()):
         willie.say("Kollege, du bist doch eingestempelt!")
         willie.say("Einmal ausstempeln, wenn du die Zeiten korrigieren willst")
         return
@@ -101,9 +137,10 @@ def add(willie, trigger):
     else:
         willie.say("Ohai thar, " + sender)
 
-    if (sender_l not in working_minutes.keys()):
+    if (sender_l not in botswana.week_list):
         willie.say(sender + " bitte verrate mir noch deine wochenarbeitszeit!")
         willie.say(sender + " wtime <Zeit in Stunden>")
+    if (sender_l not in botswana.working_minutes):
         botswana.working_minutes[sender_l] = 0
     else:
         willie.msg(sender, "Deine bisherige Wochenarbeitszeit ist " + seconds2time(botswana.working_minutes[sender_l]))
@@ -146,7 +183,10 @@ def remove(willie, trigger):
 @willie.module.nickname_commands('was geht', 'status')
 def status(willie, trigger):
     sender_l = str(trigger.nick).lower()
-    willie.say( sender + " hat heute schon " + seconds2time(time.time() - botswana.active_list[sender_l]) + " gearbeitet!")
+    if(sender_l in botswana.active_list):
+        willie.msg(sender_l, "Du hast heute schon " + seconds2time(time.time() - botswana.active_list[sender_l]) + " gearbeitet!")
+    else:
+        willie.say("Members only!")
 
 @willie.module.event('part', 'quit')
 @willie.module.rule('.*')
@@ -167,6 +207,23 @@ def join(willie, trigger):
     if(sender_l not in botswana.working_minutes):
         botswana.working_minutes[sender_l] = 0
 
+@willie.module.nickname_commands('tell')
+def addMessage(willie, trigger):
+    sender_l = (str(trigger.nick)).lower()
+    receiver_l = str(trigger.group(3)).lower()
+    if(trigger.group(3) is None):
+        willie.msg(sender_l, "tell <User> <Nachricht>")
+        return
+    
+    if(receiver_l not in botswana.working_minutes):
+        willie.msg(sender_l, "Kenn ich nicht, mach ich trotzdem!")
+        botswana.pending_messages[receiver_l] = []
+    
+    botswana.pending_messages[receiver_l].append(sender_l + " lässt ausrichten: \n" + str(trigger.group(2)))
+    
+    
+
+
 @willie.module.thread(True)
 @willie.module.interval(3600)
 def wipeWeek(willie):
@@ -174,19 +231,37 @@ def wipeWeek(willie):
         check()
     
 def setup(willie):
-    
-        schedule.every().day.at(sched_time).do(check)
-        schedule.run_pending()
-        
-        botswana = botStatus()
-        homedir = os.path.join(os.path.expanduser('~'), '.willie')
-        savedState = os.path.join(homedir, "uptime.sav")
-        if(isFile(savedState)):
-            # unpickle save file here
-            NOP
-            
-# TODO: Pickle state on shutdown
+        oldState = load(willie, None)
+        botswana.active_list = {}
+        botswana.week_list = oldState.week_list
+        botswana.working_minutes = oldState.working_minutes
+        botswana.pending_messages = oldState.pending_messages
+        botswana.overtime = oldState.overtime
 
+@willie.module.nickname_commands('.load')
+def load(willie, trigger):
+    homedir = os.path.join(os.path.expanduser('~'), '.willie')
+    savedState = os.path.join(homedir, "uptime.sav")
+    if(os.path.isfile(savedState)):
+        # unpickle save file here
+        print("Loading " + str(savedState))
+        handle = open(savedState,'rb')
+        saved = pickle.load(handle)
+        # debug:
+        print saved.week_list
+    else:
+        saved = botStatus()
+    return saved
+
+@willie.module.nickname_commands('.save')
+def shutdown(willie, trigger):
+    # save state to a pickle
+    homedir = os.path.join(os.path.expanduser('~'), '.willie')
+    savedState = os.path.join(homedir, "uptime.sav")
+    willie.msg(str(trigger.nick), "saving status to.. " + str(savedState))
+    handle = open(savedState,'wb')
+    pickle.dump(botswana, handle)
+    handle.close()
 
 if __name__ == '__main__':
     print __doc__.strip()
